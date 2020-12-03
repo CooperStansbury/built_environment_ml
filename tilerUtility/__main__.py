c392b5d39857700ff9d55f3eaeea1743c850b226
Cooper Stansbury <cstansbu@umich.edu>
2020-12-03 15:19:31 -0500

    updated random sampler

--- tilerUtility/__main__.py
+++ tilerUtility/__main__.py
@@ -26,24 +26,36 @@ def resolve_path(filepath):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="A command line interface tiling utility.")
-    parser.add_argument("-i", "--input_filepath", help="path to geotiff file for tiling.")
-    parser.add_argument("-s", "--tile_size", help="number of pixels, or side length of each tile. tiles are sqaure.")
-    parser.add_argument("-o", "--output_dir", help="output directory for saving images.")
 
-    # TODO: add option to return arrays or force conversion to png?
+    parser.add_argument("-i", "--input_filepath", help="path to geotiff file for tiling.")
+
+    parser.add_argument("-size", "--tile_size", 
+                        help="number of pixels, or side length of each tile. tiles are sqaure.")
+
+    parser.add_argument("-n", "--sample_size", nargs="?", default=None, 
+                        help="number of images to randomly sample. If not specified, all tiles returned.")
+
+    parser.add_argument("-o", "--output_dir", nargs="?", default='tilerUtility/outputs', 
+                        help="output directory for saving images. no slash `/` at the end. ")
 
     args = parser.parse_args()
-
     input_filepath = resolve_path(args.input_filepath)
     tile_size = int(args.tile_size)
     output_dir = resolve_path(args.output_dir)
 
-
     # make builder
-    # builder = quilter.MosiacBuilder(input_filepath, tile_size)
+    builder = quilter.MosiacBuilder(path=input_filepath)
 
-    # run builder 
-    # tiles, center_coords = builder.build_mosaic()
+    if not args.sample_size is None:
+        n = int(args.sample_size)
+
+        tiles, coords = builder.get_sample(n, side_len=tile_size)
+
+        builder.save_coords(coords, output_dir)
+        builder.save_tiles(tiles, output_dir)
+        builder.save_to_png(tiles, output_dir)
+    
+    # else logic is to run the entire geotiff
 
 
 
--- tilerUtility/quilter.py
+++ tilerUtility/quilter.py
@@ -17,19 +17,19 @@ from pyproj import transform
 class MosiacBuilder():
     """ A class to manage building mosaics/tiles/quilts from a geotiff images. 
     """
-    __slots__ = ['filepath', 'src', 'pad_val', 'tile_size']
-    def __init__(self, filepath, side_len):
+    __slots__ = ['path', 'src', 'pad_val', 'side_len']
+    def __init__(self, path):
         """ Initialize the builder. Note, this step is not very memory intensive. 
         
         Args:
-            - filepath (str): the full filepath to the tiled geotiff file
+            - path (str): the full filepath to the tiled geotiff file
             - side_len (int): the pixel value of the side length of a mosiac tile.
                 only squares are supported.
         """
-        self.filepath = filepath
-        self.src = rasterio.open(self.filepath)
+        self.side_len = None
+        self.path = path
+        self.src = rasterio.open(self.path)
         self.pad_val = 255 # for empty tiles on the edges of the frame
-        self.tile_size = side_len
 
 
     def get_window(self, row, col, side_len, return_win=False):
@@ -80,10 +80,44 @@ class MosiacBuilder():
         return lat, lon
 
     
-    def get_sample(self, n):    
-        """A function to get a random sample"""
-        pass
+    def get_sample(self, n, side_len):    
+        """A function to get a random sample
+        
+        Args:
+            - n (int): number of samples to return
+            - side_len (int):
 
+        Returns:
+            - tiles (list of np.array): a list of subsets
+            - center_coords (list of list): a list of [lat, lon] coordinates for the
+                center of an image
+        """
+        self.side_len = side_len
+        tiles = []
+        center_coords = []
+
+        # get random starting positions 
+        rand_rows = np.random.randint(self.src.shape[0]-side_len , size=(n,))
+        rand_cols = np.random.randint(self.src.shape[1]-side_len, size=(n,))
+
+        for row_idx, col_idx in zip(rand_rows, rand_cols):
+            # get clip
+            clip = self.get_window(row_idx, col_idx, side_len)
+
+            # handle non-square clips
+            if clip.shape[1] != side_len or clip.shape[2] != side_len:
+                pad = np.full((3, side_len, side_len), self.pad_val)
+                pad[:, 0:clip.shape[1], 0:clip.shape[2]] = clip
+                clip = pad.copy()
+
+            tiles.append(clip)
+
+            # get center lat/lon
+            lat, lon = self.get_latlon_point(row_idx + side_len // 2, col_idx + side_len // 2)
+            center_coords.append([lat, lon])
+
+        return tiles, center_coords
+    
 
     def build_mosaic(self, side_len):
         """ A method to build a set number of SQUARE
@@ -98,6 +132,7 @@ class MosiacBuilder():
             - center_coords (list of list): a list of [lat, lon] coordinates for the
                 center of an image
         """
+        self.side_len = side_len
         tiles = []
         center_coords = []
 
@@ -109,8 +144,6 @@ class MosiacBuilder():
             for j in range(int(self.src.shape[1] / side_len)+1):
 
                 clip_num += 1
-                if clip_num % 500 == 0:
-
                 # get clip
                 clip = self.get_window(row_idx, col_idx, side_len)
 
@@ -134,4 +167,100 @@ class MosiacBuilder():
         return tiles, center_coords
 
 
-   
+    def build_fig(self, size=(5, 5), dpi=300):
+        """A function to build a matplotlib figure. Primary
+        goal is to sandardize the easy stuff.
+
+        Args:
+            - size (tuple): how big should the plot be?
+            - dpi (int): the "quality" of the image
+        Returns:
+            fig (plt.figure)
+        """
+        fig = plt.figure(figsize=size, 
+                        facecolor='w',
+                        dpi=dpi)
+
+        plt.axis('off')
+        return fig
+
+    
+    def save_tiles(self, tiles, output_dir):
+        """A function to save tiles.
+
+        Args:
+            - tiles (np.array) list of tiles
+            - output_dir (str): path to save
+
+        Returns:
+            None (save only)
+        """
+        save_path = f"{output_dir}/tiles.npy"
+        tiles_np = np.asarray(tiles)
+        np.save(save_path, tiles_np)
+        print("done saving .npy!")
+
+
+    def save_coords(self, coords, output_dir):
+        """A function to save the output coordinates.
+
+        Args:
+            - coords (list): list of tile corrdinates
+            - output_dir (str): path to save
+
+        Returns:
+            None (save only)
+
+        """
+        new_rows = []
+        for i, (lat, lon) in enumerate(coords):
+            row = {
+                'tile_id': i,
+                'lat':lat,
+                'long':lon,
+                'side_length': self.side_len 
+            }
+
+            new_rows.append(row)
+
+        coord_df = pd.DataFrame(new_rows)
+        coord_df.to_csv(f"{output_dir}/coordinate_map.csv", index=False)
+        print("done saving coordinates!")
+
+
+    def save_to_png(self, tiles, output_dir, channel=None):
+        """A function to convert 3 dim RGB matrices to .png
+        files. Filename depends on order of 'tiles' input.
+
+        Args:
+            - tiles (np.array) list of tiles
+            - output_dir (str): path to save
+            - channel (int): the channel to save, or None
+
+        Returns:
+            None (save only)
+        """
+        plt.ioff()
+
+        for idx, tile in enumerate(tiles):
+            save_path = f"{output_dir}/tile_{idx}"
+            fig = self.build_fig()
+            img = np.moveaxis(tile, 0, 2)
+            
+            if channel is None:            
+                plt.imshow(img)
+                plt.savefig(save_path)
+                plt.close()
+            else:
+                plt.imshow(img[:, :, channel])
+                plt.savefig(save_path)
+                plt.close()
+
+        print("done converting to png!")
+
+
+
+
+
+
+    
